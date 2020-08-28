0cb865e6d7cad9d419707ba2687d7c5a16c8efe5
akashroy843 <royakash843@gmail.com>
2020-08-28 20:34:33 +0530

    added index.html to django

--- lunchbox/mainapp/urls.py
+++ lunchbox/mainapp/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.contact,name='contact'),
+    path('', views.index,name='index'),
     path('dev',views.developer,name='developer'),
     path('res',views.reservation,name='reservation'),
     path('abt',views.about,name='aboutus'),
     path('men',views.menu,name='menu'),
-     path('blg',views.blog,name='blog'),
+    path('blg',views.blog,name='blog'),
+    path('con',views.contact,name='contact'),
 ]
